82a4726d1b9e4a5c8d40640a557a722aebb642c6
HmimssaSoufiane <hmimssasoufiane@hotmail.com>
2020-03-13 21:25:03 +0100

    first commit

---
+++ src/test/java/com/hmimssa/catalogue/CatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmimssa.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
